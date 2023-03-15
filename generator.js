6e1db92a8443f42706cad8381b7125ec74b10748
dbryan17 <dfbryan17@gmail.com>
2023-03-15 19:45:40 -0400

    basic script working for full gen

--- generator.js
+++ generator.js
@@ -4,27 +4,34 @@ var myModule;
 var myGenPixles;
 
 createModule().then((Module) => {
-  let cgen = Module.cwrap("cgen", "null", [
+  let cgen = Module.cwrap("cgen", "number", [
     "string", // script
+  ]);
+  let getCgen = Module.cwrap("getCgen", "number", [
     "number", // string pointer
   ]);
 
   document.querySelector("#inBtn").addEventListener("click", () => {
     let input = document.querySelector("#scrIn").value.toLowerCase();
+    console.log("about to get length");
+
+    length = cgen(input);
+    console.log(length);
 
     // Trying to do this new memeory way instead of the more complex buffers
 
-    let strPtr = Module.allocateUTF8(1000); // allocate memory available to the emscripten runtime and create a pointer
+    let strPtr = Module.allocateUTF8(length); // allocate memory available to the emscripten runtime and create a pointer
     // let strPtr = Module._malloc(1000 * Uint8Array.BYTES_PER_ELEMENT);
 
-    cgen(input, strPtr);
+    type = getCgen(strPtr);
 
     code = Module.UTF8ToString(strPtr); // read from the allocated memory to the javascript string
 
+    console.log(type);
     console.log(code.trim());
     document.querySelector("#output").innerText = code.trim();
 
-    Module._free(1000); // release the allocated memory
+    Module._free(strPtr); // release the allocated memory
 
     // free the memory
   });
